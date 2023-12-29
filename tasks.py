0cf7dbf409463603669ea43a94c1231b1447753b
FlavioLionelRita <flaviolrital@hotmail.com>
2023-12-29 15:52:55 +0100

    .

---
+++ tasks.py
@@ -0,0 +1,33 @@
+"""Tasks for building and publishing the package."""
+import os
+from invoke import task, Collection
+
+ns = Collection()
+ns.configure({'run': {'executable': 'python3'}})
+
+@task
+def clean(ctx):
+    """Clean up build and distribution artifacts."""
+    ctx.run("rm -rf build dist")
+
+@task(clean)
+def build(ctx):
+    """Build the distribution package."""
+    ctx.run("python setup.py sdist bdist_wheel")
+
+@task(build)
+def test(ctx):
+    """Run tests."""
+    ctx.run("pytest")
+
+@task(test)
+def publish(ctx):
+    """Publish the package to PyPI."""
+    username = os.getenv('PYPI_USERNAME')
+    password = os.getenv('PYPI_PASSWORD')
+    if not username or not password:
+        raise ValueError("PyPI credentials not found in environment variables.")
+
+    # Aquí utiliza las credenciales en tu comando de publicación
+    cmd = f'twine upload --username "{username}" --password "{password}" dist/*'
+    ctx.run(cmd)
