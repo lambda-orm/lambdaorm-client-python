0c84fd105c8b4ed0fa0e2287bebac7b5f6c82b09
FlavioLionelRita <flaviolrital@hotmail.com>
2023-12-31 11:53:10 +0100

    test: 💍 test

---
+++ DEVELOP.md
@@ -0,0 +1,14 @@
+# Develop
+
+## Initial commit
+
+### Prerequisites
+
+```shell
+pip install commitizen
+export PATH=$PATH:/home/flavio/.local/lib/python3.10/site-packages
+```
+
+```bash
+git init
+git add .
--- tasks.py
+++ tasks.py
@@ -1,6 +1,14 @@
 """Tasks for building and publishing the package."""
 import os
+import subprocess
 from invoke import task, Collection
+
+def get_current_branch():
+    """Get the current branch name."""
+    try:
+        return subprocess.check_output(['git', 'rev-parse', '--abbrev-ref', 'HEAD'], universal_newlines=True).strip()
+    except subprocess.CalledProcessError:
+        return None
 
 ns = Collection()
 ns.configure({'run': {'executable': 'python3'}})
@@ -19,6 +27,18 @@ def build(ctx):
 def test(ctx):
     """Run tests."""
     ctx.run("pytest")
+
+@task(test)
+def release(ctx):
+    """Package and upload a release."""
+    current_branch = get_current_branch()
+    version = os.getenv('VERSION')
+    cmd= f"""git add . && git commit -m "chore(release): "{version}" && git push && git push --tags
+				  &&  git checkout -b release/"{version}" && git push --set-upstream origin release/"{version}"
+				  &&  git checkout main && git merge release/"{version}" -m "chore(release): release "{version}" " && git push
+				  &&  git checkout "{current_branch}" && git merge release/"{version}" -m "chore(release): release "{version}"" && git push
+				  &&  git branch -D release/"{version}" """
+    ctx.run(cmd)
 
 @task(test)
 def publish(ctx):
